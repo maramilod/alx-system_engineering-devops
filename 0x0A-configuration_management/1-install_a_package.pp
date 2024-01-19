67e3cd2a0e40b1334cb69791154ce38d2cb9216a
maramilod <marammilod03@gmail.com>
2024-01-19 16:30:23 +0000

    package and exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 # Installing 
 package { 'python3-pip':
- ensure => installed,
+ensure => installed,
 }
 
 exec { 'install_flask':
- command => 'pip3 install Flask==2.1.0',
- path   => ['/usr/local/bin', '/usr/bin'],
- require => Package['python3-pip'],
+command => 'pip3 install Flask==2.1.0',
+path    => ['/usr/local/bin', '/usr/bin'],
+require => Package['python3-pip'],
 }
 
 exec { 'install_werkzeug':
- command => 'pip3 install Werkzeug==2.2.1',
- path   => ['/usr/local/bin', '/usr/bin'],
- require => Package['python3-pip'],
+command => 'pip3 install Werkzeug==2.2.1',
+path    => ['/usr/local/bin', '/usr/bin'],
+require => Package['python3-pip'],
 }
 
