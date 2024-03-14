0f1ee736d7f4df2ce2c7e067a681e0fcfcbc1467
maramilod <marammilod03@gmail.com>
2024-03-14 19:36:33 +0000

    add

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# the amount of traffic
+
+# ulimit
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# restart nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
