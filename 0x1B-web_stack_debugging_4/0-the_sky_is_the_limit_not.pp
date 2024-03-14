9cbbd1a03dd8c22642e45694ff97aa2a5d3d0bc1
maramilod <marammilod03@gmail.com>
2024-03-14 19:43:17 +0000

    add

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,11 +3,11 @@
 # ulimit
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin/',
+-> }
 
 # restart nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
 }
